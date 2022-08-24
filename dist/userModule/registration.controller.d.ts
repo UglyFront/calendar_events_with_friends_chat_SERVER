bba8296736fd6f909fcaf09c6ffd5dcd925d0c30
UglyFront <76052317+UglyFront@users.noreply.github.com>
2022-08-24 23:48:22 +0300

    Delete dist directory

--- dist/userModule/registration.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { LoginDTO, RegistrationDTO } from "src/dto/auth.dto";
-import { UserEntity } from "src/entity/user.entity";
-import { RegistrationServices } from "./registration.services";
-export declare class RegistrationController {
-    private readonly registrationServices;
-    constructor(registrationServices: RegistrationServices);
-    registration(body: RegistrationDTO): Promise<UserEntity[]>;
-    login(body: LoginDTO): Promise<UserEntity[]>;
-}
--- dist/userModule/registration.services.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserEntity } from "src/entity/user.entity";
-import { Repository } from "typeorm";
-export declare class RegistrationServices {
-    private readonly userDB;
-    constructor(userDB: Repository<UserEntity>);
-    registration(dto: any): Promise<UserEntity[]>;
-    login(dto: any): Promise<UserEntity[]>;
-}
