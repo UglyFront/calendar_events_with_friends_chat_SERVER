027c69fa59252e3d9233bab98b87a63de4b99b4b
UglyFront <76052317+UglyFront@users.noreply.github.com>
2022-09-22 17:24:54 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const fs = require('fs');
 
-const key = fs.readFileSync("./key/key.pem")
-const cert = fs.readFileSync("./key/cert.pem")
+// const key = fs.readFileSync("./key/key.pem")
+// const cert = fs.readFileSync("./key/cert.pem")
 
-const httpsOptions = {
-    key: key,
-    cert: cert,
-};
+// const httpsOptions = {
+//     key: key,
+//     cert: cert,
+// };
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {httpsOptions});
+  const app = await NestFactory.create(AppModule);
   app.enableCors()
   await app.listen(80);
 }
