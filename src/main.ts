8176d5c1ed9ab82be4ad0783a25ed1f19a8c003f
UglyFront <76052317+UglyFront@users.noreply.github.com>
2022-09-27 11:42:56 +0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const fs = require('fs');
-
-// const key = fs.readFileSync("./key/key.pem")
-// const cert = fs.readFileSync("./key/cert.pem")
-
-// const httpsOptions = {
-//     key: key,
-//     cert: cert,
-// };
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(80);
+  await app.listen(6600);
 }
 bootstrap();
--- src/ws/ws.ts
+++ src/ws/ws.ts
@@ -18,22 +18,8 @@ export const db = new Pool({
 
 export class WSServer {
     constructor() {
-        const wss = new WebSocketServer({port: 443}, () => {
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            console.log(`ws start on 443`)
-            
+        const wss = new WebSocketServer({port: 6601}, () => {
+            console.log(`ws start on 6601`)
         });
 
         wss.on("connection", (ws) => {
